f3f45c6d1d7b65df67739e8478a8569ba332c8c9
Michael Naysmith <naysmithmichael@gmail.com>
2025-05-18 15:05:17 +0200

    Updated PATH_INFO parsing

--- local/cgi-bin/pathInfo.php
+++ local/cgi-bin/pathInfo.php
@@ -1,27 +1,4 @@
 <?php
-header("Content-Type: text/html");
-echo "<html><body>";
-echo "<h1>PATH_INFO Test</h1>";
-echo "<p>PATH_INFO: " . htmlspecialchars($_SERVER['PATH_INFO'] ?? 'None') . "</p>";
-
-if (!empty($_SERVER['PATH_INFO'])) {
-    $segments = explode('/', trim($_SERVER['PATH_INFO'], '/'));
-    echo "<p>Path segments:</p><ul>";
-    foreach ($segments as $segment) {
-        if (!empty($segment)) {
-            echo "<li>" . htmlspecialchars($segment) . "</li>";
-        }
-    }
-    echo "</ul>";
-}
-
-echo "<h2>All Server Variables</h2>";
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
-echo "</body></html>";
-?>
-<?php
 // path_demo.php - Simple PATH_INFO demonstration
 header("Content-Type: text/html");
 
