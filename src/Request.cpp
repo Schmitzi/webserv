cc39902a090446d870ac2cdbcdf6a9067b0a4f66
waterlillly <lillybaumeister@outlook.com>
2025-05-19 18:43:09 +0200

    FIX: error_pages and location matching

--- src/Request.cpp
+++ src/Request.cpp
@@ -164,6 +164,10 @@ void Request::parse(const std::string& rawRequest) {
     parseHeaders(headerSection);
     checkContentLength();
     parseContentType();
+
+    // if (_method == "DELETE" && _path.find("upload/") != 0) {
+    //     _path = "upload/" + _path;
+    // }
 }
 
 void Request::parseHeaders(const std::string& headerSection) {
