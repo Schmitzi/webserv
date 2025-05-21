5c2882b6a3d7b0eed4f9d1f5860a18d2685ad274
Michael Naysmith <naysmithmichael@gmail.com>
2025-05-21 14:37:10 +0200

    Simplifying Request construction

--- src/Client.cpp
+++ src/Client.cpp
@@ -253,7 +253,6 @@ int Client::processRequest(std::string &buffer) {
     }
 
     if (req.getMethod() == "BAD") {
-        std::cout << "!\n";
         std::cout << RED << _webserv->getTimeStamp() 
                 << "Bad request format" << RESET << std::endl;
         sendErrorResponse(400);
