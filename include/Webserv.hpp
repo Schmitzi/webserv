16cfda0a254a2e29f191c08222107135c4e88a94
Michael Naysmith <naysmithmichael@gmail.com>
2025-05-25 17:04:28 +0200

    Poll() to Epoll()

--- include/Webserv.hpp
+++ include/Webserv.hpp
@@ -12,12 +12,17 @@
 #include <sys/stat.h>
 #include <sstream>
 #include <ctime>
-#include <poll.h>
+// #include <poll.h>
+#include <sys/epoll.h>
 #include <iomanip>
 #include <vector>
 #include <signal.h>
 #include <map>
 #include "../include/Config.hpp"
+#include "../include/Server.hpp"
+#include "../include/Client.hpp"
+#include "../include/ConfigParser.hpp"
+#include "../include/Helper.hpp"
 
 // Forward declarations
 class Server;
@@ -31,6 +36,9 @@ class Config;
 #define WHITE   "\33[97m"
 #define RESET   "\33[0m" // No Colour
 
+// Others
+#define MAX_EVENTS 64
+
 class Webserv {
     public:
         Webserv();
@@ -40,31 +48,32 @@ class Webserv {
         ~Webserv();
 
         Server          &getServer(int i);
-        std::vector<Server> &getAllServers();
-        std::vector<struct pollfd> &getPfds();
         void            setEnvironment(char **envp);
         char            **getEnvironment() const;
         int             setConfig(std::string const filepath);
         // Polling
-        int             addToPoll(int fd, short events);
-        void            removeFromPoll(size_t index);
+        Server*         findServerByFd(int fd);
+        Client*         findClientByFd(int fd);
+        void            handleErrorEvent(int fd);
+        int             addToEpoll(int fd, short events);
+        void            removeFromEpoll(int fd);
         int             run();
         void            handleNewConnection(Server& server);
-        void            handleClientActivity(size_t pollIndex);
+        void            handleClientActivity(int clientFd);
         void            ft_error(std::string const msg);
         std::string     getTimeStamp();
         void            printMsg(const std::string msg, char const *colour, std::string const opt);
         Config          &getDefaultConfig();
+        void            cleanup();
 
     private:
         std::vector<Server *> 		_servers;
         std::vector<Client *>   	_clients;
-        std::vector<struct pollfd> 	_pfds;
         char                    	**_env;
+        int                         _epollFd;
+        struct epoll_event          _events[MAX_EVENTS];
         ConfigParser				_confParser;
-        std::vector<serverLevel>	_configs;
-
-
+        std::vector<serverLevel>	_configs; 
         
 };
 
