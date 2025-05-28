e0116512e136cb9fca76f5e5698d545d83510e41
waterlillly <lillybaumeister@outlook.com>
2025-05-28 16:45:20 +0200

    SEGFAULT BUT STARTS

--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -254,7 +254,7 @@ void ConfigParser::parseAndSetConfigs() {
                     usedCombinations.insert(combination);
                     validServer = true;
                 } else {
-                    std::cerr << RED << "Warning: Duplicate server: " << combination << RESET << std::endl;
+					throw configException("Error: Duplicate server configuration found for " + combination);
                 }
             }
         }
