70ba5df0f78eb562c21bffad0ccb05a587154299
Michael Naysmith <naysmithmichael@gmail.com>
2025-06-07 15:04:20 +0200

    Fixed merge conflicts

--- src/CGIHandler.cpp
+++ src/CGIHandler.cpp
@@ -42,28 +42,6 @@ void    CGIHandler::setCGIBin(serverLevel *config) {
     }
 }
 
-void    CGIHandler::setCGIBin(serverLevel *config) {
-    _cgiBinPath = ""; // Initialize
-    
-    std::map<std::string, locationLevel>::iterator it = config->locations.begin();
-    for (; it != config->locations.end(); ++it) {
-        if (it->first.find("php") != std::string::npos) {
-            _cgiBinPath = it->second.cgiProcessorPath;
-            break;
-        }
-    }
-    
-    if (_cgiBinPath.empty()) {
-        std::cout << RED << "Warning: No PHP CGI processor found in config" << RESET << "\n";
-        std::cout << RED << "Setting CGI-Bin to /usr/bin/php-cgi\n" << RESET;
-        _cgiBinPath = "/usr/bin/php-cgi";
-    }
-}
-
-std::string CGIHandler::getInfoPath() {
-    return _pathInfo;
-}
-
 int CGIHandler::executeCGI(Client &client, Request &req, std::string const &scriptPath) {
     cleanupResources();
     _path = scriptPath;
